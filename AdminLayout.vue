07dc232201de015820feab94ca25d12c98206b06
AliceAda <2877878883@qq.com>
2026-08-20 19:59:52 +0800

    chore: 移动布局文件到正确位置

--- AdminLayout.vue
+++
@@ -1,9 +0,0 @@
-// AdminLayout.vue - 后台管理布局
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'AdminLayout',
-  render() {
-    return null
-  }
-})
--- LandlordLayout.vue
+++
@@ -1,9 +0,0 @@
-// LandlordLayout.vue - 房东中心布局
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'LandlordLayout',
-  render() {
-    return null
-  }
-})
